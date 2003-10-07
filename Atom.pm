4db5201ffd3a1756010273ed7f5abeb9b40e05c4
Ivan Tubert-Brohman <ivan.tubert@gmail.com>
2003-10-07 01:01:31 +0000

    Added the rest of the elements

--- Atom.pm
+++ Atom.pm
@@ -54,6 +54,12 @@ my $N = 0; # Number of atoms created so far, used to generate default IDs.
     Na  Mg                                          Al  Si  P   S   Cl  Ar
     K   Ca  Sc  Ti  V   Cr  Mn  Fe  Co  Ni  Cu  Zn  Ga  Ge  As  Se  Br  Kr
     Rb  Sr  Y   Zr  Nb  Mo  Tc  Ru  Rh  Pd  Ag  Cd  In  Sn  Sb  Te  I   Xe
+    Cs  Ba
+        La  Ce  Pr  Nd  Pm  Sm  Eu  Gd  Tb  Dy  Ho  Er  Tm  Yb
+            Lu  Hf  Ta  W   Re  Os  Ir  Pt  Au  Hg  Tl  Pb  Bi  Po  At  Rn
+    Fr  Ra
+        Ac  Th  Pa  U   Np  Pu  Am  Cm  Bk  Cf  Es  Fm  Md  No
+            Lr  Rf  Db  Sg  Bh  Hs  Mt  Ds  Uuu Uub Uut Uuq Uup Uuh Uus Uuo
 );
 
 for (my $i = 1; $i < @ELEMENTS; ++$i){
